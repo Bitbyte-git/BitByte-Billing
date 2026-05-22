435cfd7be9dbc99da419ee6b72ba5c8e4c8e0d64
Roshini <devroshini84@gmail.com>
2026-05-22 18:10:37 +0530

    fixed api base url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': 'https://bitbyte-server.onrender.com'
+      '/api': 'https://bitbyte-server.onrender.com/api'
     }
   }
 });
