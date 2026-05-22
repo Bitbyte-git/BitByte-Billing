f9e44d6a50ba2b997468c0e6a6e8db75dcec15e4
Bitbyte-git <reachus@bitbytetech.org>
2026-05-22 09:22:18 +0530

    Delete BitByte-Billing directory

--- BitByte-Billing/server/src/middleware/validate.js
+++
@@ -1,7 +0,0 @@
-import { validationResult } from 'express-validator';
-
-export function validate(req, _res, next) {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) return next(Object.assign(new Error('Validation failed'), { status: 422, details: errors.array() }));
-  next();
-}
